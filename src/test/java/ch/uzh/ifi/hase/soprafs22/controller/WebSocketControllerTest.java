2d17487709db2f764371115348b8691e60d8a16b
Najma Christen <najma@gmx.ch>
2022-04-29 18:21:16 +0200

    prepared WebSocketControllerTest

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/controller/WebSocketControllerTest.java
@@ -0,0 +1,23 @@
+package ch.uzh.ifi.hase.soprafs22.controller;
+
+public class WebSocketControllerTest {
+
+    //TODO /hello
+
+    //TODO /game
+
+    //TODO /start
+
+    //TODO /discard
+
+    //TODO /draw
+
+    //TODO /gameLost
+
+    //TODO /gameLeft
+
+    //TODO /gameStatus
+
+
+
+}
