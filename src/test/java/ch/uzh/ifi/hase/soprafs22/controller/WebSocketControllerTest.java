41a3e45701b458b78ab680a17592e8a2e3fee247
Sandra Rosch <sandra.rosch@uzh.ch>
2022-04-30 16:21:02 +0200

    added some small testing ideas and json

--- src/test/java/ch/uzh/ifi/hase/soprafs22/controller/WebSocketControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/controller/WebSocketControllerTest.java
@@ -12,6 +12,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.StringMessageConverter;
 import org.springframework.messaging.simp.stomp.StompFrameHandler;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
@@ -64,11 +66,17 @@ public class WebSocketControllerTest {
         allPlayers.add(player1);
         allPlayers.add(player2);
         given(waitingRoom.getPlayerList()).willReturn(allUsers);
-*/
-        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue(1);
 
+*/
+        //Setup before each
         this.webSocketStompClient = new WebSocketStompClient(new SockJsClient(
                 List.of(new WebSocketTransport(new StandardWebSocketClient()))));
+
+
+        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue(1);
+
+        webSocketStompClient.setMessageConverter(new StringMessageConverter());
+        Thread.sleep(1000);
 
 
         StompSession session = webSocketStompClient
@@ -95,7 +103,14 @@ public class WebSocketControllerTest {
         session.send("/app/start", null);
 
         System.out.println("blockingQueue: "+ blockingQueue);
-        //  assertEquals(, blockingQueue.poll(1, TimeUnit.SECONDS));
+          //assertEquals(, blockingQueue.poll(1, TimeUnit.SECONDS));
+
+        // {"noCardsOnDeck":84,"whoseTurn":"Anna","pilesList":[{"topCard":{"value":100},"direction":"TOPDOWN"},
+        // {"topCard":{"value":100},"direction":"TOPDOWN"},{"topCard":{"value":1},"direction":"DOWNUP"},
+        // {"topCard":{"value":1},"direction":"DOWNUP"}],"playerCards":{"Peter":[{"value":39},{"value":92},{"value":78},{"value":80},{"value":44},{"value":23},{"value":46}],
+        // "Anna":[{"value":58},{"value":91},{"value":75},{"value":15},{"value":93},{"value":52},{"value":63}]},"gameRunning":true}
+        // Maybe we can access the gameStauts variable in the game object and  just check if "gameRunning" is true
+        // so that we don't have to mock all the cards
     }
 
     //TODO /discard
