c82a71cb1bfa0dbd0223a5b01ecfc7459d98f70a
Deborah Stebler <deborah.stebler@uzh.ch>
2022-05-06 08:47:16 +0200

    Made the Handcards sorted

--- src/main/java/ch/uzh/ifi/hase/soprafs22/entity/Card.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/entity/Card.java
@@ -1,7 +1,18 @@
 package ch.uzh.ifi.hase.soprafs22.entity;
 
-public class Card {
+public class Card implements Comparable<Card> {
     private int value;
+
+    @Override
+    public int compareTo(Card anotherCard) {
+        if (this.value < anotherCard.value) {
+            return -1;
+        }
+        if (this.value > anotherCard.value) {
+            return 1;
+        }
+        return 0;
+    }
 
     public Card(int value){
         this.value = value;
--- src/main/java/ch/uzh/ifi/hase/soprafs22/entity/Player.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/entity/Player.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.hase.soprafs22.entity;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 
@@ -29,8 +30,8 @@ public class Player {
     public Deck fillCards(int fillTo, Deck deck){
         while(getNoOfCards() < fillTo && deck.getNoOfCards() > 0){
             handCards.add(deck.pop());
-            //deck.removeCardOnDeck();
         }
+        handCards.sort(Comparator.naturalOrder());
         return deck;
     }
 
